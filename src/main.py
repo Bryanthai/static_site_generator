5876c56ac2f2f18b4e2109dfc69197d37717e1a2
Hahant <theblacksheepp4@gmail.com>
2025-04-17 16:58:05 +0800

    github pages

--- src/main.py
+++ src/main.py
@@ -1,33 +1,30 @@
 import os
 import shutil
+import sys
 from textnode import TextNode, TextType
 from htmlnode import LeafNode, HTMLNode, ParentNode
 from convert import *
-
-def check_path():
-    if not os.path.exists("public"):
-        raise Exception("Public path does not exist")
-    return
 
 def initialize(current_path):
     content_list = os.listdir(os.path.join("static", current_path))
     for content in content_list:
         static_content = os.path.join("static", current_path,  content)
         if os.path.isdir(static_content):
-            public_path = os.path.join("public", current_path, content)
+            public_path = os.path.join("docs", current_path, content)
             os.mkdir(public_path)
             initialize(os.path.join(current_path, content))
         else:
-            public_path = os.path.join("public", current_path, content)
+            public_path = os.path.join("docs", current_path, content)
             shutil.copy(static_content, public_path)
     return
 
 def nuke_public_initialize():
-    shutil.rmtree("public")
-    os.mkdir("public")
+    if os.path.isdir("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
     return
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"GENERATING PAGE FROM {from_path} TO {dest_path} USING {template_path}")
     f = open(from_path, "r")
     from_content = f.read()
@@ -38,29 +35,34 @@ def generate_page(from_path, template_path, dest_path):
     html_title = extract_title(from_content)
     title_change = re.sub("{{ Title }}", html_title, template_content)
     content_change = re.sub("{{ Content }}", html_content, title_change)
-    check_path()
+    href_change = re.sub("href=\"", f"href=\"{base_path}", content_change)
+    src_change = re.sub("src=\"", f"src=\"{base_path}", href_change)
     d = open(dest_path, "w")
-    d.write(content_change)
+    d.write(src_change)
     return
 
-def generate_page_recursive(template_path, current_path):
+def generate_page_recursive(template_path, base_path, current_path):
     content_list = os.listdir(os.path.join("content", current_path))
     for content in content_list:
         static_content = os.path.join("content", current_path,  content)
         if os.path.isdir(static_content):
-            public_path = os.path.join("public", current_path, content)
+            public_path = os.path.join("docs", current_path, content)
             os.mkdir(public_path)
-            generate_page_recursive(template_path, os.path.join(current_path, content))
+            generate_page_recursive(template_path, base_path, os.path.join(current_path, content))
         elif static_content.endswith(".md"):
             html_content = re.sub(".md", ".html", content)
-            public_path = os.path.join("public", current_path, html_content)
-            generate_page(static_content, template_path, public_path)
+            public_path = os.path.join("docs", current_path, html_content)
+            generate_page(static_content, template_path, public_path, base_path)
     return
 
 def main():
+    if len(sys.argv) < 2:
+        base_path = ""
+    else:
+        base_path = sys.argv[1]
     nuke_public_initialize()
     initialize("")
-    generate_page_recursive("template.html", "")
+    generate_page_recursive("template.html", base_path, "")
 
 
 main()
